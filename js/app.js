5ffae9f19dc3164cc39ad1d7d36f124b17b64eb6
Sérgio Lopes <sergio.lopes@caelum.com.br>
2015-07-28 23:41:50 -0300

    modal

--- js/app.js
+++ js/app.js
@@ -14,3 +14,21 @@ $('.collection')
         $(this).remove();
         return false;
     });
+
+$('.modal-trigger').leanModal();
+
+$('#confirmar').on('click', function() {
+    var texto = "";
+
+    $('.badge').parent().each(function(){
+        texto += this.firstChild.textContent + ': ';
+        texto += this.lastChild.textContent + ', ';
+    });
+
+    $('#resumo').empty().text(texto);
+});
+
+$('.acao-limpar').on('click', function() {
+    $('#numero-mesa').val('');
+    $('.badge').remove();
+});
