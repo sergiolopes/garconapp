a97c7d93cfe9a9c0536496a5ce2bf0516fb85ec7
Sérgio Lopes <sergio.lopes@caelum.com.br>
2015-08-06 23:13:47 -0300

    barcode

--- www/js/app.js
+++ www/js/app.js
@@ -32,3 +32,17 @@ $('.acao-limpar').on('click', function() {
     $('#numero-mesa').val('');
     $('.badge').remove();
 });
+
+$('.scan-qrcode').on('click', function(){
+    cordova.plugins.barcodeScanner.scan(
+       function (resultado) {
+           if (resultado.text) {
+               Materialize.toast('Mesa ' + resultado.text, 2000);
+               $('#numero-mesa').val(resultado.text);
+           }
+       },
+       function (error) {
+           Materialize.toast('Erro: ' + error, 3000, 'red-text');
+       }
+    );
+});
